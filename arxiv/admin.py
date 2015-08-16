521eb598abed2f0387c1f7395dd5b611b2e731ae
Grigory Feldman <fgb631@yandex.ru>
2015-08-16 15:33:38 +0300

    Add admin model

--- arxiv/admin.py
+++ arxiv/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
+from .models import Article
+
+from .models import Painter
+
+from .models import Author
+
+from .models import Category
+
+from .models import Magazine
+
+
+
+
+admin.site.register(Painter)
+
+admin.site.register(Author)
+
+admin.site.register(Category)
+
+admin.site.register(Magazine)
+
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_authors', 'magazine')
+    list_filter = ['magazine']
+
+admin.site.register(Article, ArticleAdmin)
+
 # Register your models here.
--- arxiv/models.py
+++ arxiv/models.py
@@ -9,8 +9,8 @@ class Creator(models.Model):
 	name = models.CharField(max_length=50)
 	surname = models.CharField(max_length=50)
 	photo = models.ImageField(upload_to='articles_img', blank=True)
-	site = models.EmailField(max_length=254)
-	info = models.CharField(max_length=1000)
+	site = models.EmailField(max_length=254, blank=True)
+	info = models.CharField(max_length=1000, blank=True)
 
 	def __str__(self):
 		return self.surname
@@ -20,10 +20,10 @@ class Creator(models.Model):
 
 
 class Author(Creator):
-	authorinfo = models.CharField(max_length=500)
+	authorinfo = models.CharField(max_length=500, blank=True)
 
 class Painter(Creator):
-	painterinfo = models.CharField(max_length=500)
+	painterinfo = models.CharField(max_length=500, blank=True)
 
 
 YEARS = [(i, i) for i in range(2011, 2050)]
@@ -32,7 +32,7 @@ MONTHS = [(i, i) for i in range(1, 13)]
 class Magazine(models.Model):
 	year = models.IntegerField(choices=YEARS)
 	month = models.IntegerField(choices=MONTHS)
-	cover = models.ImageField(upload_to='covers')
+	cover = models.ImageField(upload_to='covers', blank=True)
 
 	def __str__(self):
 		return str(self.month) + '/' + str(self.year)  
@@ -50,12 +50,14 @@ class Category(models.Model):
 class Article(models.Model):
 	title = models.CharField(max_length=255)
 	authors = models.ManyToManyField(Author)
+	def get_authors(self):
+		return '\n'.join([p.surname for p in self.authors.all()])
 	painters = models.ManyToManyField(Painter, blank=True)
 	magazine = models.ForeignKey(Magazine)
 	category = models.ForeignKey(Category)
-	pdf = models.FileField(upload_to='articles_pdf')
+	pdf = models.FileField(upload_to='articles_pdf', blank=True)
 	slug = models.CharField(max_length=200)
-	teaser = models.CharField(max_length=1000)
+	teaser = models.CharField(max_length=1000, blank=True)
 	preview = models.ImageField(upload_to='articles_preview', blank=True)
 	access = models.BooleanField()
 	similar = models.ManyToManyField('self', blank=True)
